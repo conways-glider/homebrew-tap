1845d42d00adfd3051a8711c0390d1d5573e7cc7
Nia <7388349+conways-glider@users.noreply.github.com>
2022-07-31 21:30:05 +0000

    ct: add 1.1.0 bottle.

--- Formula/ct.rb
+++ Formula/ct.rb
@@ -5,6 +5,12 @@ class Ct < Formula
   sha256 "9057f120e2676eefb350fcea506aa3c7aa34e79b0e5fd1634c64c48a3d18ccf9"
   license any_of: ["MIT", "Apache-2.0"]
 
+  bottle do
+    root_url "https://github.com/conways-glider/homebrew-tap/releases/download/ct-1.1.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "437464c9c4fbba58d57b726e6effa03c31090c8a2cea636ea31630667f5a6fab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0414455a6a2dd3ce471fcf1fc3a7cda7754188270f74f7b989fc92ddb773b6f6"
+  end
+
   depends_on "go" => :build
 
   def install
